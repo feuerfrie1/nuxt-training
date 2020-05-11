1f413e42494e600be55cbd8916d1b5a544ca7e58
Anastasiia Tychkova <tychkovaam@gmail.com>
2020-05-10 18:06:06 -0700

    added DoubledProgressBar and adjusted styles

--- components/ui/ProgressBar.vue
+++ components/ui/ProgressBar.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="progressBar">
-    <div class="progressBar__fill"></div>
+    <div class="progressBar__fill" :style="style"></div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    value: Number,
+    maxValue: Number,
+  },
+  computed: {
+    style() {
+      return `width: ${(this.value / this.maxValue) * 100}%`;
+    },
+  },
+};
 </script>
 
 <style scoped>
@@ -14,11 +24,13 @@ export default {};
   min-height: 40px;
   background-color: #f4f4f4;
   display: flex;
+  position: relative;
 }
 .progressBar__fill {
   background-color: #613a93;
   position: absolute;
   left: 0;
   top: 0;
+  height: 100%;
 }
 </style>
